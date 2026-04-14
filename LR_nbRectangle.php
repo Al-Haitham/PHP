b572f35e3c9e54f91a81c4e930f595d66e838a8b
鹰Al-Haitam <study.haitham@gmail.com>
2026-04-14 13:11:22 +0100

    new PHP files

---
+++ LR_nbRectangle.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Numbered Triangle</title>
+</head>
+<body>
+    <form method="get">
+        <label for="nbr">Nombre des Lignes:</label>
+        <input type="number" name="nbr" id="nbr" value="<?php echo isset($_GET['nbr']) ? $_GET['nbr'] : ''; ?>">
+        <button type="submit">Generate</button>
+    </form>
+
+    <div class="result">
+        <?php
+        if (isset($_GET['nbr']) && $_GET['nbr'] > 0) {
+            $nbr = (int)$_GET['nbr'];
+            for ($i = 1; $i <= $nbr; $i++) {
+                for ($j = 1; $j <= $i; $j++) {
+                    echo $j;
+                }
+                echo "<br>";
+            }
+        }
+        ?>
+    </div>
+</body>
+</html>   
