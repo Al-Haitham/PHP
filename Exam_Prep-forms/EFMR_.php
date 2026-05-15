6f7a801bbb177a42433b04f2d1cf72e5c41822a6
鹰Al-Haitam <study.haitham@gmail.com>
2026-05-15 20:00:04 +0100

    new linesPOO

---
+++ Exam_Prep-forms/EFMR_.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        class agence{
+            private $codeAg;
+            private $nomAg;
+            private $adresseAg;
+            private $telAg;
+            public function __constructor($codeAg,$nomAg,$adresseAg,$telAg){
+                $this->codeAg=
+                $this->nomAg=$nomAg;
+                $this->adresseAg=$adresseAg;
+                $this->telAg=$telAg;
+            }
+            public function getcodeAg($this){
+                return $this->codeAg;
+             }
+             public function getNomAg($this){
+                return $this->nomAg;
+             }
+
+        }
+
+    ?>
+</body>
+</html>
