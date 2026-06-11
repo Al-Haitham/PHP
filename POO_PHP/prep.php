d411aaf8a747c2c486f779650f9c12734849a6d7
鹰Al-Haitam <study.haitham@gmail.com>
2026-06-11 20:37:07 +0100

    fixes

---
+++ POO_PHP/prep.php
@@ -0,0 +1,21 @@
+<?php
+class professeur{
+    private int $CodePro;
+    private string $NomPro;
+    private string $PrenomPro;
+    private string $DateNaissance;
+    private string $Grade;
+
+    public function __construct(int $CodePro,string $NomPro,string $PrenomPro,string $DateNaissance,string $Grade){
+        $this->CodePro=$CodePro;
+        $this->NomPro=$NomPro;
+        $this->PrenomPro=$PrenomPro;
+        $this->DateNaissance=$DateNaissance;
+        $this->Grade=$Grade;
+    }
+
+    public function 
+
+}
+
+?>
